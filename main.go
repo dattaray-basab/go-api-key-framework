0b333bb51e48cd5861913e7011b94bcd7ba21b02
Basab Dattaray <dattaraybasab0@gmail.com>
2025-01-11 15:35:30 -0800

    curl tested

--- main.go
+++ main.go
@@ -24,3 +24,25 @@ func main() {
 		log.Fatalf("Failed to start server: %v", err)
 	}
 }
+
+
+// to test using curl
+// curl -X POST http://localhost:8080/generate -H "Content-Type: application/json" -d '{"user_id": "test_user"}'
+// curl -X GET "http://localhost:8080/validate?api_key=<YOUR_API_KEY>"
+// curl -X POST http://localhost:8080/revoke -H "Content-Type: application/json" -d '{"api_key": "<YOUR_API_KEY>"}'
+
+// To test the API key management system, you can use the following curl commands:
+// Actual STEPS shown below:
+// 1.
+// bd@Basabs-MBP go-api-key-framework % curl -X POST http://localhost:8080/generate -H "Content-Type: application/json" -d '{"user_id": "test_user"}'
+
+// {"api_key":"NlINIdDZfT0y6RxKRVGC1cYWWK9XMPO5tSFdPaYOC_A="}% 
+
+// 2.
+// bd@Basabs-MBP go-api-key-framework % curl -X GET "http://localhost:8080/validate?api_key=NlINIdDZfT0y6RxKRVGC1cYWWK9XMPO5tSFdPaYOC_A="
+// {"message":"API key is valid"}%    
+
+// 3.
+// bd@Basabs-MBP go-api-key-framework % curl -X POST http://localhost:8080/revoke -H "Content-Type: application/json" -d '{"api_key": "NlINIdDZfT0y6RxKRVGC1cYWWK9XMPO5tSFdPaYOC_A="}'
+// {"message":"API key revoked"}%   
+
